e5c4034b25974fddae01168186c5f747f4f2cc2f
ceasaro <cees@crop-r.com>
2015-12-26 11:25:20 +0100

    added install_requires

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     author_email='ceesvw@gmail.com',
     packages=find_packages(),
     include_package_data=True,
+    install_requires=['GitPython==1.0.1',],
     url='https://github.com/ceasaro/about_py',
     license='',
     description='creates an about page containing VSC information, python/python libs versions, etc.',
